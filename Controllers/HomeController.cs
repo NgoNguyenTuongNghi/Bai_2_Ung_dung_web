f939c8bdf44ec97cac4ebdbb1ed1722852199ed8
Tường Nghi <62927599+NgoNguyenTuongNghi@users.noreply.github.com>
2020-04-24 16:31:50 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register2(FormCollection data)
+        {
+            ViewBag.maSV = data["maSV_name"];
+            ViewBag.hoten = data["hoten_name"];
+            ViewBag.diem = data["diem_name"];
+            return View(ViewBag);
+        }
+    }
+}
